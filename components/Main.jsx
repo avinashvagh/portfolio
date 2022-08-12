0c2fbcc70d8e14d6bc67c48f033fac4edc1b5a82
avinashvagh <avinashvagh0001@protonmail.com>
2022-08-12 15:55:33 +0530

    done with main-page |

---
+++ components/Main.jsx
@@ -0,0 +1,43 @@
+
+
+import React from 'react'
+import { FaGithub, FaLinkedinIn } from 'react-icons/fa';
+import { AiOutlineMail } from 'react-icons/ai';
+import { BsFillPersonLinesFill } from 'react-icons/bs';
+
+function Main() {
+  return (
+    <div className='w-full h-screen text-center'>
+        <div className="max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center">
+             <div>
+               <p className='uppercase text-sm tracking-widest text-gray-600'>LET&#39;S BUILD SOMETHING TOGETHER</p>
+               <h1 className='py-4 text-gray-700'> Hi, I&#39;m <span className='text-[#34A6A7]'>Avinash</span></h1>
+               <h1 className='py-2 text-gray-700'> A Front-End Developer</h1>
+               <p className='py-4 text-gray-600 max-w-[70%] m-auto'>
+                I&#39;m a front-end developer specializing in building user interfaces and experiences. I have a passion for creating clean, elegant, and intuitive user interfaces. Currently, I&#39;m focused on building responsive front-end web applications while learning back-end technologies.
+
+                I&#39;m a bootcamp-taught developer and I&#39;m always looking to learn new technologies and techniques. 
+               </p>
+               <div className="flex items-center justify-between max-w-[330px] m-auto py-4">
+                     <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-105 ease-in duration-300'>
+                        <FaLinkedinIn/>
+                     </div>
+                     <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-105 ease-in duration-300'>
+                        <FaGithub/>
+                     </div>
+                     <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-105 ease-in duration-300'>
+                        <AiOutlineMail/>
+                     </div>
+                     <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-105 ease-in duration-300'>
+                        <BsFillPersonLinesFill/>
+                     </div>
+
+               </div>
+
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Main
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -84,7 +84,7 @@ const Navbar = () => {
             </ul>
 
             <div className="pt-40">
-              <p className="uppercase tracking-widest text-[#37a3a9]" >Let&#39;s connect </p>
+              <p className="uppercase tracking-widest text-[#34A6A7]" >Let&#39;s connect </p>
 
               <div className="flex items-center justify-between my-4 w-full sm:w-[80%]">
                 <div className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300">
